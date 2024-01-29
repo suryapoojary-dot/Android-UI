0a12902e3dccaf55fff432d1caeaa2d610b4f9fa
Surya Poojary <77871514+Gh0st-sniper@users.noreply.github.com>
2024-01-29 18:28:36 +0530

    added findViewByID for both cards

--- app/src/main/java/com/mochacookiecrumble/acitvity_and_intent/MainActivity.java
+++ app/src/main/java/com/mochacookiecrumble/acitvity_and_intent/MainActivity.java
@@ -1,14 +1,20 @@
 package com.mochacookiecrumble.acitvity_and_intent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView starterscard ;
+    CardView mainscard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        starterscard = findViewById(R.id.text_view_restaurant_menu);
+        mainscard = findViewById(R.id.text_main_menu);
     }
 }
