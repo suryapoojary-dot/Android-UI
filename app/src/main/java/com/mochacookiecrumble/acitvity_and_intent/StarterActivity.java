5edea727d96fdaba3119db944e01474cfeaf276e
Surya Poojary <77871514+Gh0st-sniper@users.noreply.github.com>
2024-01-30 17:35:22 +0530

    Added List View

--- app/src/main/java/com/mochacookiecrumble/acitvity_and_intent/StarterActivity.java
+++ app/src/main/java/com/mochacookiecrumble/acitvity_and_intent/StarterActivity.java
@@ -3,6 +3,7 @@ package com.mochacookiecrumble.acitvity_and_intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class StarterActivity extends AppCompatActivity {
 
@@ -10,6 +11,8 @@ public class StarterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starter);
+
+        ListView starterslist = findViewById(R.id.list_view_starters);
 
 
     }
