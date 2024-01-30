69d716815f1d19acdf1db670a588acee5ac271ff
Surya Poojary <77871514+Gh0st-sniper@users.noreply.github.com>
2024-01-30 21:56:50 +0530

    Added food items to dishes

--- app/src/main/java/com/mochacookiecrumble/acitvity_and_intent/StarterActivity.java
+++ app/src/main/java/com/mochacookiecrumble/acitvity_and_intent/StarterActivity.java
@@ -7,7 +7,31 @@ import android.widget.ListView;
 
 public class StarterActivity extends AppCompatActivity {
 
-    String[] dishes = {"Chicken Crispy", "Egg 65", "Chicken Manchow", "Veg Spring Roll"};
+    String[] dishes = {
+            "Chicken Crispy",
+            "Egg 65",
+            "Chicken Manchow",
+            "Veg Spring Roll",
+            "Mushroom and tofu maki",
+            "Egg and avocado uramaki",
+            "Melon and lemon soup",
+            "Coconut and chocolate mousse",
+            "Spinach and cabbage wontons",
+            "Broccoli and cucumber soup",
+            "Chilli and aubergine dip",
+            "Chickpea and chilli gyoza",
+            "Sprout and pineapple soup",
+            "Egusi and borscht soup",
+            "Aubergine and egg sushi",
+            "Artichoke and mustard soup",
+            "Peppercorn and tamarind soup",
+            "Parsley and celeriac parcels",
+            "Pasta and broccoli soup",
+            "Potato and courgette soup",
+            "Chickpea and cabbage parcels",
+            "Coriander and peppercorn gyoza",
+            "Pear and chestnut soup",
+            "Pesto and garam masala parcels"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
