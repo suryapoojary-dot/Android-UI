09ab4234e50df9d6b71e37a633bc8229130fae9a
Surya Poojary <77871514+Gh0st-sniper@users.noreply.github.com>
2024-01-30 22:34:36 +0530

    Created Dish class

---
+++ app/src/main/java/com/mochacookiecrumble/acitvity_and_intent/Dish.java
@@ -0,0 +1,14 @@
+package com.mochacookiecrumble.acitvity_and_intent;
+
+public class Dish {
+
+    String title;
+    String description;
+    int price;
+
+    Dish(String title,String description,int price) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+    }
+}
